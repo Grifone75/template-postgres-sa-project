77e9a5e194b7dd340308dc789cde190a91f673c2
Fabrizio Gambino <fabrizio.gambino@doktor.se>
2023-03-17 09:32:57 +0100

    repositioned models folder

--- alembic/env.py
+++ alembic/env.py
@@ -16,9 +16,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from src.models.base import Base
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -62,7 +62,6 @@ def run_migrations_online() -> None:
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
