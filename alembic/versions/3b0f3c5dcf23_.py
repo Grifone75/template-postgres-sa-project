9f07788c15147671a64277a3b8729e916255f064
Fabrizio Gambino <fabrizio.gambino@doktor.se>
2023-03-17 09:33:29 +0100

    added two revisions

---
+++ alembic/versions/3b0f3c5dcf23_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3b0f3c5dcf23
+Revises: 
+Create Date: 2023-03-17 09:23:22.849965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3b0f3c5dcf23'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
